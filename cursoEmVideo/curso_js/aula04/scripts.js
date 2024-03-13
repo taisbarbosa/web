46b3bfdecd232cfe3c027f4352bbe668a2ca1048
taisbarbosa <taisbarbosarodrigues2004@gmail.com>
2024-03-13 17:43:07 -0300

    android

---
+++ cursoEmVideo/curso_js/aula04/scripts.js
@@ -0,0 +1,8 @@
+/*var nome = window.prompt('Qual o seu nome?')
+window.alert('É um prazer te conhecer '+nome+'!')*/
+
+var n1 = Number(window.prompt('Digite um número: '))
+var n2 = Number(window.prompt('Digite mais um número: '))
+var s = n1 + n2
+
+window.alert('A soma dos valores é '+s)
